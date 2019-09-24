6af287c783128c7aaa3af2cd66cba645324a63a2
anderson.lopes@altran.com <anderson.lopes@altran.com>
2019-09-24 21:39:28 +0100

    Inclui modelo oportunidade.

--- src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
+++ src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
@@ -1,16 +1,27 @@
 package com.lopes.comercial.controller;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lopes.comercial.model.Oportunidade;
 
 @RestController
 @RequestMapping("/oportunidades")
 public class OprtunidadeController {
 
 	@GetMapping
-	public String listar() {
-		return "Hello!";
+	public List<Oportunidade> listar() {
+		Oportunidade oportunidade = new Oportunidade();
+		oportunidade.setId(1L);
+		oportunidade.setNomeProspecto("Desenvolvimento de ERP com Angular e Spring");
+		oportunidade.setDescricao("Altran Portugal");
+		oportunidade.setValor(new BigDecimal(345000));
+		return Arrays.asList(oportunidade);
 	}
 
 }
