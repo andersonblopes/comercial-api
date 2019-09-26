2337509f2df60a8101ef64db940a0f18e409f6d5
anderson.lopes@altran.com <anderson.lopes@altran.com>
2019-09-26 16:05:37 +0100

    Remove task.

--- src/main/java/com/lopes/comercial/repository/OportunidadeRepository.java
+++ src/main/java/com/lopes/comercial/repository/OportunidadeRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lopes.comercial.model.Oportunidade;
 
-public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long>{
-	
-	//TODO Definir métodos de consultas personalizadas.
-
+public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
 }
