b272ed36d937bbd2b85f703ad9d7e692daa70187
anderson.lopes@altran.com <anderson.lopes@altran.com>
2019-09-26 17:30:12 +0100

    Insere oportunidade na base de dados.

--- src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
+++ src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,11 +29,16 @@ public class OprtunidadeController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Oportunidade> obterOportunidade(@PathVariable Long id) {
-		Optional<Oportunidade> oportunidade = oportunidadeRepository.findById(id); 
-		if(!oportunidade.isPresent()) {
+		Optional<Oportunidade> oportunidade = oportunidadeRepository.findById(id);
+		if (!oportunidade.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(oportunidade.get());
+	}
+
+	@PostMapping
+	public Oportunidade inserir(@RequestBody Oportunidade oportunidade) {
+		return oportunidadeRepository.save(oportunidade);
 	}
 
 }
