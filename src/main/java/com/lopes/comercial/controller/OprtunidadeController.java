82947b03ec7727a07cf71be465faed4e9f26a6f9
anderson.lopes@altran.com <anderson.lopes@altran.com>
2019-09-26 16:50:24 +0100

    Inclui busca de oportunidade pelo id.

--- src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
+++ src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
@@ -1,9 +1,12 @@
 package com.lopes.comercial.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,15 @@ public class OprtunidadeController {
 	@GetMapping
 	public List<Oportunidade> listar() {
 		return oportunidadeRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Oportunidade> obterOportunidade(@PathVariable Long id) {
+		Optional<Oportunidade> oportunidade = oportunidadeRepository.findById(id); 
+		if(!oportunidade.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(oportunidade.get());
 	}
 
 }
