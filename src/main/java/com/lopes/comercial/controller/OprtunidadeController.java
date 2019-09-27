d34a71c14cda441d2031ce1d0c5342c78bc3196b
anderson.lopes@altran.com <anderson.lopes@altran.com>
2019-09-27 11:50:46 +0100

    Remove oportunidade.

--- src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
+++ src/main/java/com/lopes/comercial/controller/OprtunidadeController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,7 +55,8 @@ public class OprtunidadeController {
 		return oportunidadeRepository.save(oportunidade);
 	}
 
-	public void remover(Long id) {
+	@DeleteMapping("/{id}")
+	public void remover(@PathVariable Long id) {
 		oportunidadeRepository.deleteById(id);
 	}
 
