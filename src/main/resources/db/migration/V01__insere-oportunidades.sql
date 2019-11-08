67fc3ae11be3131cd04f9a9b6289b9e2c4d763ae
Anderson Lopes <alopes@digitalis.pt>
2019-11-08 13:21:05 +0000

    Atualização de configurações.

--- src/main/resources/db/migration/V01__insere-oportunidades.sql
+++ src/main/resources/db/migration/V01__insere-oportunidades.sql
@@ -7,4 +7,7 @@ create table oportunidade (
   primary key (id)
 );
 
-insert into oportunidade (nome_prospecto, descricao, valor) values ('Altran Portugal', 'Desenvolvimento de ERP com Angular e Spring', 50000);
+insert into oportunidade (nome_prospecto, descricao, valor) values ('Digitalis', 'Desenvolvimento de ESB', 950000);
+insert into oportunidade (nome_prospecto, descricao, valor) values ('Digitalis', 'Desenvolvimento de ERP com Angular e Spring', 056510);
+insert into oportunidade (nome_prospecto, descricao, valor) values ('Digitalis', 'Suporte Técnico', 200000);
+insert into oportunidade (nome_prospecto, descricao, valor) values ('Digitalis', 'Customização de Sistema', 5000);
